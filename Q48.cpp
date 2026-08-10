3168b90645da338f9c9a67f44f29c0369c5898bf
Veer Pratap Singh <xyresiiic@gmail.com>
2026-08-10 14:12:33 +0530

    added program

--- Q48.cpp
+++ Q48.cpp
@@ -7,16 +7,30 @@ class Solution
     public:
     void rotate(vector<vector<int>>& matrix) 
     {
+        //Tanspose the matrix
         int n = matrix.size();
-        for (int i = 0; i < n / 2; i++) 
+        for (int i = 0; i < n; i++) 
         {
-            for (int j = i; j < n - i - 1; j++) 
+            for (int j = i; j < n; j++) 
             {
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[n - j - 1][i];
-                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
-                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
-                matrix[j][n - i - 1] = temp;
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //rotate the matrix by
+        for(int i = 0; i < n; i++) 
+        {
+            int left = 0;
+            int right = n - 1;
+            while(left < right) 
+            {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
             }
         }
     }
@@ -39,3 +53,4 @@ class Solution
 
         return 0;
     }
+
