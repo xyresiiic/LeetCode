a3496b29f64976c0f09cb6d0ef0c490fdc4ebe98
Veer Pratap Singh <xyresiiic@gmail.com>
2026-08-10 14:05:24 +0530

    added program

---
+++ Q48.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).*/
+
+class Solution 
+{
+    public:
+    void rotate(vector<vector<int>>& matrix) 
+    {
+        int n = matrix.size();
+        for (int i = 0; i < n / 2; i++) 
+        {
+            for (int j = i; j < n - i - 1; j++) 
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+};
+
+    int main() 
+    {
+        Solution solution;
+        vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        solution.rotate(matrix);
+
+        for (const auto& row : matrix) 
+        {
+            for (const auto& elem : row) 
+            {
+                cout << elem << " ";
+            }
+            cout << endl;
+        }
+
+        return 0;
+    }
