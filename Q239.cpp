bcffb8e84bfcac1fd613b8456320e0406c60f0fd
Veer Pratap Singh <xyresiiic@gmail.com>
2026-09-21 14:30:05 +0530

    update

---
+++ Q239.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            while (!dq.empty() && dq.front() <= i - k)
+                dq.pop_front();
+
+            while (!dq.empty() && nums[dq.back()] <= nums[i])
+                dq.pop_back();
+
+            dq.push_back(i);
+
+            if (i >= k - 1)
+                ans.push_back(nums[dq.front()]);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 3, -1, -3, 5, 3, 6, 7};
+    int k = 3;
+    vector<int> result = solution.maxSlidingWindow(nums, k);
+
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
